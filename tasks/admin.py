0a15973c04ae98fd75ba1848b305fe1c20eb2ad9
Arash Pourebrahimi <arashpor00000@gmail.com>
2021-04-09 22:08:14 +0430

    create views
    create daily and routine tasks

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import DailyTasks, RoutineTasks
 
-# Register your models here.
+admin.site.register(DailyTasks)
+admin.site.register(RoutineTasks)
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from .models import *
 
 
 def index(request):
-    return render(request, 'tasks/index.html')
+    daily_tasks = DailyTasks.objects.all()
+    daily_form = DailTasksForm()
+    routine_tasks = RoutineTasks.objects.all()
+    routine_form = RoutineTasksForm()
+    context = {'DailyTasks': daily_tasks, 'RoutineTasks': routine_tasks,
+               'DailyForm': daily_form, 'RoutineForm': routine_form}
+    if request.method == "POST":
+        if request.POST.get('create daily task'):
+            form = DailTasksForm(request.POST)
+            if form.is_valid():
+                form.save()
+            return redirect("/")
+    if request.method == "POST":
+        if request.POST.get('create routine task'):
+            form = RoutineTasksForm(request.POST)
+            if form.is_valid():
+                form.save()
+            return redirect("/")
+    return render(request, 'tasks/index.html', context)
